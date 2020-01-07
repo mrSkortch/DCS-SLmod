b5dfc6945f7abbbd423523bcb39662757f4e2635
mrSkortch <jeff.szorc@gmail.com>
2020-01-06 21:21:15 -0700

    Minor Vote Fixes
    -Changed RTV to require >= the value needed to rock the vote rather than simply being only greater than.
    
    -Fixed error on successful vote if the mission had .miz in the name when adding to totalVoteLoaded in metaStats.

--- Scripts/net/Slmodv7_5/SlmodVote.lua
+++ Scripts/net/Slmodv7_5/SlmodVote.lua
@@ -114,7 +114,7 @@ do
         if rtvStatus and rtvStatus.startedAt > 0 and rtvStatus.active == true then -- there is an RTV vote currently going on
             local currentVote = countVotes(rtvTbl)
             
-            if currentVote > slmod.num_clients * rtvStatus.level then
+            if currentVote >= slmod.num_clients * rtvStatus.level then
                 slmod.scheduleFunctionByRt(slmod.basicChat, {'Slmod: RTV has been succsseful. Mission vote now starting. Vote will last for: ' .. getVoteTimeRemainingInMin(voteStatus.maxVoteTime) .. ' minutes.'}, DCS.getRealTime() + 0.1)
                 rtvReset(true)
                 return
@@ -186,9 +186,10 @@ do
                         else
                             -- 
                             slmod.scheduleFunctionByRt(slmod.scopeMsg, {'Slmod: Vote has been successful! ' .. leader .. ' has won the vote. The server will be switching to it in a moment.', 10, 'both' }, DCS.getRealTime() + 0.1)  -- scheduled so that reply from Slmod appears after your chat message.
-                            
-                            if mStats.missionStats[leader].totalVoteLoaded then
-                                 slmod.stats.changeMetaStatsValue(mStats.missionStats[leader],'totalVoteLoaded', mStats.missionStats[leader].totalVoteLoaded + 1)
+
+                            local nameString = string.gsub(leader, '.miz', '')
+                            if mStats.missionStats[nameString] and mStats.missionStats[nameString].totalVoteLoaded then
+                                 slmod.stats.changeMetaStatsValue(mStats.missionStats[nameString],'totalVoteLoaded', mStats.missionStats[nameString].totalVoteLoaded + 1)
                             end
                             slmod.scheduleFunctionByRt(net.load_mission, {path .. leader}, DCS.getRealTime() + 10)
                         end
